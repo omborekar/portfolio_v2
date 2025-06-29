97e39f28ffcd0b6f921397de8b5eacfe82945204
Om Borekar <omborekar18@gmail.com>
2025-06-29 16:41:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/portfolio/', // <-- Add this line with your repo name
+  base: '/portfolio_v2/', // <-- Add this line with your repo name
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
